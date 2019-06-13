f1e0c5667d19f954875c560231aada8c55a3d1ea
FelizpeLima <felipe0709saopaulo@icloud.com>
2019-06-13 03:26:58 -0300

    Adding like controller, post controller and upload images

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
@@ -8,6 +9,11 @@ mongoose.connect(
   {
     useNewUrlParser: true
   }
+);
+
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
 );
 
 app.use(require("./routes"));
